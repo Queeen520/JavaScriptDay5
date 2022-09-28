03c3617582b608ed16088ddace70963160a905a6
Isabella W <isabellawindhaber@gmail.com>
2022-09-28 12:17:09 +0200

    classes

---
+++ ISABELLA_SILIA/basic_classes/EX1_Isabella.js
@@ -0,0 +1,39 @@
+/* Create 3 different classes:  car, animal and person. These classes should have some properties like 
+name, brand, model, type, color, weight, age... of course inherent to its type because, for instance, an animal has no brand... 
+
+Each class should have at least one method, that shows some properties about each theme. Feel free to expand on that.
+
+Create, using the classes, 3 different objects for each class.
+
+The results from the methods must be printed on the browser too. */
+
+
+
+class car = {
+
+        "type":,
+        "name":,
+        "price": "€348.670",
+        "Horsepower": 729,
+        "colors": ["black", "white", "red", "yellow"],
+        
+        constructor (type, name) {
+        this.type = Supercar;
+        this.name = Ferrari F8;
+       }
+
+       "summary": function() {
+        return this.type + " " + this.name;
+    }
+}
+
+
+
+class animal = {
+
+}
+
+
+class person = {
+
+}
